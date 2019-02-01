659a20b6b70b61d5b7e9ceb949d3c3aaa346c1ca
jaxBCD <oke.wae906@gmail.com>
2019-02-01 21:25:04 +0700

    Zeebsploit-framework

---
+++ zeebsploit.py
@@ -0,0 +1,30 @@
+#-*- coding: utf-8 -*-
+# https://github.com/jaxBCD
+# codename : jaxbcd
+# facebook : https://www.facebook.com/jaka.lesmana.794628
+# Team : 407 Authentic exploit
+from __future__ import print_function
+import sys,os
+from zsf.zeeb_mank import *
+print_function
+
+if sys.platform == 'win32':
+   print('This Tool only For Linux Sorry :)')
+   exit('Sorry')
+
+if sys.version[0] == '2':
+   print('\n')
+   print('[!] This Tool Only support for python 3.x')
+   print('\n')
+   sys.exit()
+else:
+   while True:
+         beb = input('[?] Do you want to check For update ? [y/n] ').lower()
+         if beb == 'y':
+            os.system('git pull')
+            print('[!] Type python zeebsploit.py to run the tool\n')
+         elif beb == 'n':
+            main()
+         else:
+            print('[!] Type "y" or "n" only')
+
---
+++ zsf/core/helper.py
@@ -0,0 +1,64 @@
+#-*- coding: utf-8 -*-
+from prettytable import PrettyTable
+
+class Helper:
+
+
+      def infoga_helper():
+          pt = PrettyTable()
+          pt.field_names = ['Modules','Description']
+          infoga_modules = {'Detect_cms':'a tool for detecting cms on a web',
+                            'Port_Scanner':'Scan Open Port use Nmap',
+                            'ip_Geolocation':'Scan Location Host or Ip',
+                            'Whois':'request and response protocol for web data',
+                            'Email_Extractor':'EXtract Email from web',
+                            'Traceroute':'to show the route the package has passed',
+                            'Robot.Txt':'Scan Robot.txt from Web',
+                            'header_check':'Response Header Checker',
+                            '---More---':'Coming Soon the following version'}
+          for dat,ls in infoga_modules.items():
+              pt.add_row([dat,ls])
+          print(pt)
+
+      def scanner_helper():
+          pt = PrettyTable()
+          pt.field_names = ['Modules','Description']
+          scanner_modules = {'Subdomain_Scanner':'Scan Subdomain for Web',
+                             'SQLi_Scanner':'Scan Sql Injection Vulnerability',
+                             'XSS_Scanner':'Scan XSS Injection Vulnerability',
+                             'LFI_Scanner':'Local File Includes Scanner etc/passwd',
+                             'Admin_finder':'Scan Admin Login page',
+                              '---More---':'Coming Soon the following version'}
+
+          for dat,ls in scanner_modules.items():
+              pt.add_row([dat,ls])
+          print(pt)
+
+
+      def exploit_modules():
+          pt = PrettyTable()
+          pt.field_names = ['Modules','Description']
+          exploit_modules = {'Webdav_File_Upload':'--None--',
+                             'Dvr_cam_leak_credential':'TBK DVR4104 / DVR4216 - Credentials Leak (Get User and password',
+                             'Wp_Content_Injection':'WP Content Injection File upload version:4.7 and 4.7.1',
+                             'Wp_Revslider':'Wordpress Revslider Plugin Shell Auto Upload',
+                             'Wp_showbiz':'Wordpress Showbiz Pro shell Auto Upload',
+                             'Wp_Learndash':'Wordpress Learndash shell Auto Upload',
+                             'Joomla_com_fabrik':'Joomla Component_Fabrik Auto Shell Upload',
+                             '---More---':'Coming Soon the following version'}
+
+          for dat,ls in exploit_modules.items():
+              pt.add_row([dat,ls])
+          print(pt)
+
+      def utama():
+          pt = PrettyTable()
+          pt.field_names = ['Modules','Description']
+          utama = {'Exploit':'Exploitation Modules',
+                   'Scanners':'Scanners Modules',
+                   'infoga':'information Gathering Modules'}
+
+          for dat,la in utama.items():
+              pt.add_row([dat,la])
+          print(pt)
+
